d8a04ef86136a4ca2376f29eb9418014acca997f
Junhyeok Lee <jhl221123@naver.com>
2025-05-11 20:49:34 +0900

    test set -e

---
+++ src/main/java/mre/cicd_set_e/CicdSetEApplication.java
@@ -0,0 +1,13 @@
+package mre.cicd_set_e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSetEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSetEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mre/cicd_set_e/CicdSetEApplicationTests.java
@@ -0,0 +1,13 @@
+package mre.cicd_set_e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSetEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
